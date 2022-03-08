2ae084b31836689ad3107a550e386a20f825d548
tomonne <hagimarble@gmail.com>
2022-03-08 21:19:03 +0900

    1章 - 配列・オブジェクト
    課題

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,17 @@
+//問1
+let scores = [10, 15, 20, 25];
+console.log(scores);
+for (let i = 0; i < scores.length; i++){
+    if (scores[i] % 2 === 0){
+        console.log(scores[i] + "は偶数です");
+    }
+}
+
+//問2
+let car = {
+    gass: 20.5,
+    num: 1234
+}
+console.log(car);
+console.log("ガソリンは" + car.gass + "です");
+console.log("ナンバーは" + car.num + "です");
