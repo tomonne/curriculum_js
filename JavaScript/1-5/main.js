4b00f144e266db71bc65c0ca19a1b9a33036b96e
tomonne <hagimarble@gmail.com>
2022-03-09 14:35:13 +0900

    1章 - 関数とは?
    課題再提出

--- JavaScript/1-5/main.js
+++ JavaScript/1-5/main.js
@@ -4,10 +4,10 @@ function createJuice(fruits) {
     //③「〇〇を受け取りました。ジュースにして返します」と出力
     console.log(fruits + "を受け取りました。ジュースにして返します");
     //④受け取った果物にジュースという文字列を結合して、呼び出し元に返す
-    juice = (fruits + "ジュース");
+    let juice = (fruits + "ジュース");
     return juice;
 }
 //⑤関数の実行：「みかん」を渡して、結果を受け取れるように適当な変数（orangeJuice）を用意
 //⑥返ってきた変数を利用して「〇〇が届きました」と出力する
 let orangeJuice = createJuice('みかん');
-console.log(juice + "が届きました");
+console.log(orangeJuice + "が届きました");
