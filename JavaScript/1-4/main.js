018e5062624a1bb9f0459228e588aa0016bb3648
tomonne <hagimarble@gmail.com>
2022-03-09 13:53:47 +0900

    1章 - 繰り返しの処理
    課題再提出

--- JavaScript/1-4/main.js
+++ JavaScript/1-4/main.js
@@ -1,17 +1,13 @@
 let number = 1;
 while (number <= 100){
-    if (number % 3 != 0 && number % 5 != 0){
+    if (number % 3 === 0 && number % 5 === 0){
+        console.log("FizzBuzz!");
+    } else if (number % 3 === 0){
+        console.log("Fizz!");
+    } else if (number % 5 === 0){
+        console.log("Buzz!");
+    } else {
         console.log(number);
-    } else{
-        if (number % 3 === 0){
-            if (number % 5 === 0){
-                console.log("FizzBuzz!");
-            } else{
-                console.log("Fizz!");
-            }
-        } else if (number % 5 === 0){
-            console.log("Buzz!");
-        }
     }
     number++;
 }
